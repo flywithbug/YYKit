efe47c4822a2f7621f388540525684aacbe17a0c
flywithbug <flywithbug@gmail.com>
2018-06-02 00:35:32 +0800

    no message

--- YYKit/YYKit.h
+++ YYKit/YYKit.h
@@ -138,8 +138,6 @@ FOUNDATION_EXPORT const unsigned char YYKitVersionString[];
 #import "CALayer+YYAdd.h"
 #import "YYCGUtilities.h"
 
-#import "NSObject+YYModel.h"
-#import "YYClassInfo.h"
 
 #import "YYCache.h"
 #import "YYMemoryCache.h"
